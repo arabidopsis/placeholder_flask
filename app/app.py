cd7654f10690943cb7621bdd7086acf30d74a795
arabidopsis <ian.castleden@gmail.com>
2024-03-13 09:31:54 +0800

    redirect

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 
 app = Flask(__name__)
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<path>')
-def index(path=''):
+def index(path=""):
+    if path != "":
+        return redirect(url_for('.index'))
     return render_template('index.html')
 
 
